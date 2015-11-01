b6b00a98ae84347e2e1389f486de3ce4cb267eab
Danny <dmayer@dekarmedia.com>
2015-11-01 12:43:42 -0800

    Tracking Buttons for website
    CSS/GA tracking implemented for Kyle and Steve

--- functions.php
+++ functions.php
@@ -2,6 +2,17 @@
 
 <?php
 function ga_tracking_one() {
-return '<div class="vcex-blog-entry-readmore-wrap clr textcenter"><a href="'. bp_loggedin_user_domain() . 'messages/compose/?r=wreesmandm" title="Send a private message to this user." class="send-message vcex-button animate-on-hover">Private Message David & Melody</a></div>';
+
+echo "<a class=\"button medium button custom fusion-button button-3d button-square button-medium button-custom button-1 buttonshadow-0\" type=\"button\" target=\"_blank\" title=\"\" href=\"http://www.napaonline.com/Catalog/Result.aspx?Ntt%3dENEOS%26Ntk%3dKeyword%26Nty%3d1%26Dn%3d0%26D%3dENEOS%26Dk%3d1%26Dp%3d3%26N%3d0\" onClick=\"ga('send', 'event', { eventCategory: 'Purchase', eventAction: 'Button', eventLabel: 'Kyle-Larson'});><span class=\"fusion-button-text\">Purchase Now</span></a>\n";
 }
-add_shortcode('ga-tracking-one', 'ga_tracking_one');
+
+add_shortcode('ga-tracking-kyle', 'ga_tracking_one');
+
+
+function ga_tracking_two() {
+
+echo "<a class=\"button medium button custom fusion-button button-3d button-square button-medium button-custom button-1 buttonshadow-0\" type=\"button\" target=\"_blank\" title=\"\" href=\"http://www.napaonline.com/Catalog/Result.aspx?Ntt%3dENEOS%26Ntk%3dKeyword%26Nty%3d1%26Dn%3d0%26D%3dENEOS%26Dk%3d1%26Dp%3d3%26N%3d0\" onClick=\"ga('send', 'event', { eventCategory: 'Purchase', eventAction: 'Button', eventLabel: 'Steve-Arpin'});><span class=\"fusion-button-text\">Purchase Now</span></a>\n";
+
+}
+
+add_shortcode('ga-tracking-steve', 'ga_tracking_two');
